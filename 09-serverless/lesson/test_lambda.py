127a3fba2043530f98dfb95dfd29ce0f7acb4621
Erick <edcm.erick@gmail.com>
2023-11-15 17:42:02 -0500

    update url

--- 09-serverless/lesson/test_lambda.py
+++ 09-serverless/lesson/test_lambda.py
@@ -2,7 +2,7 @@ import requests
 import json
 
 url = 'http://localhost:8080/2015-03-31/functions/function/invocations'
-url = 'https://6nv7u5n4sj.execute-api.us-east-2.amazonaws.com/clothes-api/predict'
+#url = 'https://6nv7u5n4sj.execute-api.us-east-2.amazonaws.com/clothes-api/predict'
 
 event = {'url': 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQJs8cWy35WqJWtYd1EJREiTdT3_qkP33-bTA&usqp=CAU'}
 
